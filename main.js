70db156e082a6d4e3d1a1bc2b4eef52e72e8702c
Akaehomen peace <peacedavid049@gmail.com>
2023-10-15 18:21:01 +0100

    commit

--- main.js
+++ main.js
@@ -1,19 +1,30 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-  const modal = document.getElementById('myModal');
-  const closeModal = document.getElementById('closeModal');
+// window.addEventListener('DOMContentLoaded', (event) => {
+ // const modal = document.getElementById('myModal');
+ // const closeModal = document.getElementById('closeModal');
 
-  modal.style.display = 'block';
+ // modal.style.display = 'block';
 
-  closeModal.addEventListener('click', () => {
-      modal.style.display = 'none';
-  });
+ // closeModal.addEventListener('click', () => {
+ //     modal.style.display = 'none';
+ // });
 
-});
+//});
 
-const videoTag = document.getElementById('video-bg');
+//const videoTag = document.getElementById('video-bg');
 
-window.addEventListener('load', () => {
-    setInterval(()=>{
-        videoTag.muted = false;
-    }, 3000)
+//window.addEventListener('load', () => {
+ //   setInterval(()=>{
+ //       videoTag.muted = false;
+ //   }, 3000)
+//})
+
+const hamburger = document.getElementById('nav');
+const navigation = document.getElementById('navigation');
+const chevron = document.getElementById('close-btn');
+
+
+//change navbar styles on scroll
+
+window.addEventListener('scroll', () => {
+  document.querySelector('.desktop-nav').classList.toggle('window-scroll', window.scrollY > 100);
 })
